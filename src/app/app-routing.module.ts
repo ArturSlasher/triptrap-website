e10822f0fe3831ff8c0060433dba65fe7234d1f2
ArturSlasher <archiblablablapicher@gmail.com>
2021-09-12 21:26:36 +0300

    created routing for auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+
+const routes: Routes = [
+  { path: 'signup', component: SignupPageComponent},
+  { path: 'login', component: LoginPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
