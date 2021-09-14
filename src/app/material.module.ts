212fe959b21f25e819687bca54b13e4d7a5e4654
ArturSlasher <archiblablablapicher@gmail.com>
2021-09-14 22:20:37 +0300

    completed header layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from "@angular/core";
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatFormFieldModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   exports: [
     MatButtonModule,
     MatFormFieldModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatIconModule,
+    MatBadgeModule
   ]
 })
 
