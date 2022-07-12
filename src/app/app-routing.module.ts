f172e5ce98c4f383eb16c4b098d5c45cd4205e37
ArturSlasher <archiblablablapicher@gmail.com>
2022-07-12 20:31:41 +0300

    changed folders structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginPageComponent } from './login-page/login-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { SignupPageComponent } from './auth/signup-page/signup-page.component';
 import { HomeComponent } from './home/home.component';
 import { MyTripsComponent } from './my-trips/my-trips.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { LoginFormComponent } from './login-page/login-form/login-form.component';
-import { IllustrationsComponent } from './login-page/illustrations/illustrations.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { SignupFormComponent } from './signup-page/signup-form/signup-form.component';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { LoginFormComponent } from './auth/login-page/login-form/login-form.component';
+import { IllustrationsComponent } from './auth/illustrations/illustrations.component';
+import { SignupPageComponent } from './auth/signup-page/signup-page.component';
+import { SignupFormComponent } from './auth/signup-page/signup-form/signup-form.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './home/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeCardsComponent } from './home/home-cards/home-cards.component';
-import { NavtabsComponent } from './home/header/navtabs/navtabs.component';
+import { NavtabsComponent } from './header/navtabs/navtabs.component';
 import { MyTripsComponent } from './my-trips/my-trips.component';
 import { TripsGroupComponent } from './my-trips/trips-group/trips-group.component';
 import { UserCardComponent } from './my-trips/trips-group/user-card/user-card.component';
 import { TripsListComponent } from './my-trips/trips-group/trips-list/trips-list.component';
 import { TripContentSectionComponent } from './my-trips/trip-content-section/trip-content-section.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { TripContentSectionComponent } from './my-trips/trip-content-section/tri
     UserCardComponent,
     TripsListComponent,
     TripContentSectionComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
